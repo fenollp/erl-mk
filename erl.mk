acf37e11bb1970b849d0b84e52abec19d4ed1ea0
Pierre Fenoll <pierrefenoll@gmail.com>
2013-11-19 12:07:30 +0000

    Mainly added compilation support for *.core and *.S files.

--- erl.mk
+++ erl.mk
@@ -1,17 +1,18 @@
 all: ebin project
 .PHONY: all
 
-#ALL_DEPS_DIRS = $(addprefix deps/,$(DEPS))
-
 ####
 
-#deps : $(ALL_DEPS_DIRS)
+ALL_DEPS_DIRS = $(addprefix deps/,$(DEPS))
+
+get-deps : $(ALL_DEPS_DIRS)
+
+deps/%/:
+	git clone -n 
+#	+$(MAKE) -C $@
 
 #clean-deps:
 #	$(foreach dep,$(DEPS),+$(MAKE) clean -C $(dep);)
-
-#deps/%/:
-#	+$(MAKE) -C $@
 
 ####
 
@@ -26,6 +27,12 @@ ebin/%.beam: src/%.yrl
 	erlc -o ebin/ $<
 	erlc -o ebin/ ebin/$*.erl
 
+ebin/%.beam: src/%.S
+	erlc $(ERL_OPTS) -v +from_asm -o ebin/ -pa ebin/ -I include/ $<
+
+ebin/%.beam: src/%.core
+	erlc $(ERL_OPTS) -v +from_core -o ebin/ -pa ebin/ -I include/ $<
+
 ebin/%.app: src/%.app.src
 	cp $< $@
 
@@ -33,9 +40,11 @@ APPSRC = $(patsubst src/%.app.src,ebin/%.app,$(wildcard src/*.app.src))
 ERLS = $(patsubst src/%.erl,ebin/%.beam,$(wildcard src/*.erl))
 XRLS = $(patsubst src/%.xrl,ebin/%.beam,$(wildcard src/*.xrl))
 YRLS = $(patsubst src/%.yrl,ebin/%.beam,$(wildcard src/*.yrl))
+ASMS = $(patsubst src/%.S,ebin/%.beam,$(wildcard src/*.S))
+CORES = $(patsubst src/%.core,ebin/%.beam,$(wildcard src/*.core))
 
-project: $(APPSRC) $(ERLS) $(XRLS) $(YRLS)
-	echo $?
+project: $(APPSRC) $(ERLS) $(XRLS) $(YRLS) $(ASMS) $(CORES)
+#	echo $?
 
 ebin:
 	mkdir ebin/
