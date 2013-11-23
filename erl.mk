d45a95cd732701519640a742abaebeef6a4537da
Pierre Fenoll <pierrefenoll@gmail.com>
2013-11-23 14:25:05 +0000

    Removed some debug code.

--- erl.mk
+++ erl.mk
@@ -24,7 +24,6 @@ app: $(patsubst src/%.app.src,ebin/%.app, $(wildcard src/*.app.src)) \
      $(patsubst src/%.yrl,    ebin/%.beam,$(wildcard src/*.yrl    )) \
      $(patsubst src/%.S,      ebin/%.beam,$(wildcard src/*.S      )) \
      $(patsubst src/%.core,   ebin/%.beam,$(wildcard src/*.core   ))
-#	echo $?
 .PHONY: app
 
 ebin/%.beam: src/%.erl      | ebin/
