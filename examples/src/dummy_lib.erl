75a0589f89453608440b048c6b5c693bf3ac8723
Pierre Fenoll <pierrefenoll@gmail.com>
2013-12-07 17:24:49 +0000

    Dummy example setup.

---
+++ examples/src/dummy_lib.erl
@@ -0,0 +1,20 @@
+%% See LICENSE for licensing information.
+%% -*- coding: utf-8 -*-
+-module(dummy_lib).
+
+%% dummy_lib: dummy_lib library's entry point.
+
+-export([my_func/0]).
+
+
+%% API
+
+my_func () ->
+    ok().
+
+%% Internals
+
+ok () ->
+    ok.
+
+%% End of Module.
