b20f51c7e01579652ab4aec9c843168d0613e3ac
Pierre Fenoll <pierrefenoll@gmail.com>
2014-09-23 21:43:31 +0200

    Fix tidbits in samples/

--- samples/lagering.git/src/lagering.erl
+++ samples/lagering.git/src/lagering.erl
@@ -9,6 +9,7 @@
 %% API
 
 my_func() ->
+    {ok,_} = application:ensure_all_started(lager),
     lager:info("~s is ~s!", [lager, cool]),
     lager:warning("but pay ~s!", [attention]),
     lager:error("there is always some ~s", [error]),
